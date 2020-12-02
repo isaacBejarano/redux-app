b36b25c828ecdee911f2f1935615deacf36117c1
isaacBejarano <isaacbejaranopro@gmail.com>
2020-12-02 11:10:22 +0100

    Karma-Jasmine first test on app - navbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  // global <Promise> init ..........
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let ref: HTMLElement;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    ref = fixture.nativeElement;
+  });
+  // ..............
+
+  it('app shell exists', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'redux-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('redux-app');
+  it(`title1 is 'redux-app'`, () => {
+    expect(app.title1).toEqual('redux-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`title2 is 'ngRx = Redux + RxJS'`, () => {
+    expect(app.title2).toEqual('ngRx = Redux + RxJS');
+  });
+
+  it('renders textContent Home', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('redux-app app is running!');
+    expect(ref.querySelector('.nav-item:nth-child(1)').textContent).toContain(
+      'Home'
+    );
+  });
+
+  it('renders textContent Link', () => {
+    fixture.detectChanges();
+    expect(ref.querySelector('.nav-item:nth-child(2)').textContent).toContain(
+      'Link'
+    );
   });
 });
