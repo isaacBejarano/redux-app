073d0c10c73d1c865a75172c8f7e9211c34c3ae0
isaacBejarano <isaacbejaranopro@gmail.com>
2020-12-03 01:14:01 +0100

    deviuos implementation of emmiter from lvl3 to prop of lvl2 to pass up to parent

--- src/app/emitters/child/child.component.ts
+++ src/app/emitters/child/child.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent {
+  @Input() counterLvl2: number; // prop
+  @Output() counterEmitLvl2 = new EventEmitter<number>(); // prop
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  // methods
+  halve(): void {
+    this.counterEmitLvl2.emit(this.counterLvl2 / 2);
   }
 
+  double(): void {
+    this.counterEmitLvl2.emit(this.counterLvl2 * 2);
+  }
+
+  // we have to emit to parent by using a method.
+  // the parent cannot read directly the outputed prop in child
+  counterEmitFromLvl3(valueFromLvl3: number): void {
+    this.counterEmitLvl2.emit(valueFromLvl3);
+  }
 }
--- src/app/emitters/grandchild/grandchild.component.ts
+++ src/app/emitters/grandchild/grandchild.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-grandchild',
   templateUrl: './grandchild.component.html',
-  styleUrls: ['./grandchild.component.scss']
+  styleUrls: ['./grandchild.component.scss'],
 })
-export class GrandchildComponent implements OnInit {
+export class GrandchildComponent {
+  @Input() counterLvl3: number; // prop
+  @Output() counterEmitLvl3 = new EventEmitter<number>(); // prop
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  // methods
+  reset(): void {
+    this.counterEmitLvl3.emit(0);
   }
-
 }
